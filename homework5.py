9a7144c9c1a50ab66cd3e697c117393289c32938
Antwil1988 <easy_snail@inbox.ru>
2024-07-22 22:15:40 +0700

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -1,6 +1,6 @@
 # Создали переменную и присвоили ей кортеж из нескольких элементов разных типов данных.
 immutable_var= 41, 'text', 3.23, False
-print(immutable_var)
+print('Immutable tuple:',immutable_var)
 
 #immutable_var [0] = [99]
 # Объяснение:
@@ -11,4 +11,4 @@ print(immutable_var)
 mutable_list = [1, 'cold', 'apple', 4.1]
 mutable_list[1] = 222  # Изменяем второй элемент списка
 mutable_list [3] = 'fish' # Изменяем четвертый элемент списка
-print(mutable_list) # Вывод списка на экран
+print('Mutable list:',mutable_list) # Вывод списка на экран
